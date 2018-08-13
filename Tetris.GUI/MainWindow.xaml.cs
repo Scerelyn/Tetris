e5961e4610f41980aa4c31aa46fd7f65049ba455
KingofPawns <dvandeveer@student.neumont.edu>
2018-08-13 14:40:59 -0600

    Forgot to add gui to repo in previous commit

---
+++ Tetris.GUI/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Tetris.GameEngine;
+
+namespace Tetris.GUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        Game tetris = new Game();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                Tetrid.ColumnDefinitions.Add(new ColumnDefinition());
+            }
+            for (int i = 0; i < 20; i++)
+            {
+                Tetrid.RowDefinitions.Add(new RowDefinition());
+            }
+            
+
+        }
+    }
+}
